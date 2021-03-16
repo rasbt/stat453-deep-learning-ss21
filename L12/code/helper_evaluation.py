7e869f801608c85f9809ebbb1973be13695793cc
rasbt <mail@sebastianraschka.com>
2021-03-16 12:09:45 -0500

    code for lecture 12

---
+++ L12/code/helper_evaluation.py
@@ -0,0 +1,44 @@
+# imports from installed libraries
+import os
+import numpy as np
+import random
+import torch
+from distutils.version import LooseVersion as Version
+
+
+def set_all_seeds(seed):
+    os.environ["PL_GLOBAL_SEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+def set_deterministic():
+    if torch.cuda.is_available():
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+    if torch.__version__ <= Version("1.7"):
+        torch.set_deterministic(True)
+    else:
+        torch.use_deterministic_algorithms(True)
+
+
+def compute_accuracy(model, data_loader, device):
+
+    with torch.no_grad():
+
+        correct_pred, num_examples = 0, 0
+
+        for i, (features, targets) in enumerate(data_loader):
+
+            features = features.to(device)
+            targets = targets.float().to(device)
+
+            logits = model(features)
+            _, predicted_labels = torch.max(logits, 1)
+
+            num_examples += targets.size(0)
+            correct_pred += (predicted_labels == targets).sum()
+    return correct_pred.float()/num_examples * 100
---
+++ L12/code/helper_plotting.py
@@ -0,0 +1,97 @@
+# imports from installed libraries
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+
+def plot_training_loss(minibatch_loss_list, num_epochs, iter_per_epoch,
+                       results_dir=None, averaging_iterations=100):
+
+    plt.figure()
+    ax1 = plt.subplot(1, 1, 1)
+    ax1.plot(range(len(minibatch_loss_list)),
+             (minibatch_loss_list), label='Minibatch Loss')
+
+    if len(minibatch_loss_list) > 1000:
+        ax1.set_ylim([
+            0, np.max(minibatch_loss_list[1000:])*1.5
+            ])
+    ax1.set_xlabel('Iterations')
+    ax1.set_ylabel('Loss')
+
+    ax1.plot(np.convolve(minibatch_loss_list,
+                         np.ones(averaging_iterations,)/averaging_iterations,
+                         mode='valid'),
+             label='Running Average')
+    ax1.legend()
+
+    ###################
+    # Set scond x-axis
+    ax2 = ax1.twiny()
+    newlabel = list(range(num_epochs+1))
+
+    newpos = [e*iter_per_epoch for e in newlabel]
+
+    ax2.set_xticks(newpos[::10])
+    ax2.set_xticklabels(newlabel[::10])
+
+    ax2.xaxis.set_ticks_position('bottom')
+    ax2.xaxis.set_label_position('bottom')
+    ax2.spines['bottom'].set_position(('outward', 45))
+    ax2.set_xlabel('Epochs')
+    ax2.set_xlim(ax1.get_xlim())
+    ###################
+
+    plt.tight_layout()
+
+    if results_dir is not None:
+        image_path = os.path.join(results_dir, 'plot_training_loss.pdf')
+        plt.savefig(image_path)
+
+
+def plot_accuracy(train_acc_list, valid_acc_list, results_dir):
+
+    num_epochs = len(train_acc_list)
+
+    plt.plot(np.arange(1, num_epochs+1),
+             train_acc_list, label='Training')
+    plt.plot(np.arange(1, num_epochs+1),
+             valid_acc_list, label='Validation')
+
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.tight_layout()
+
+    if results_dir is not None:
+        image_path = os.path.join(
+            results_dir, 'plot_acc_training_validation.pdf')
+        plt.savefig(image_path)
+
+
+def show_examples(model, data_loader):
+
+    for batch_idx, (features, targets) in enumerate(data_loader):
+
+        with torch.no_grad():
+            features = features
+            targets = targets
+            logits = model(features)
+            predictions = torch.argmax(logits, dim=1)
+        break
+
+    fig, axes = plt.subplots(nrows=3, ncols=5,
+                             sharex=True, sharey=True)
+
+    nhwc_img = np.transpose(features, axes=(0, 2, 3, 1))
+    nhw_img = np.squeeze(nhwc_img.numpy(), axis=3)
+
+    for idx, ax in enumerate(axes.ravel()):
+        ax.imshow(nhw_img[idx], cmap='binary')
+        ax.title.set_text(f'P: {predictions[idx]} | T: {targets[idx]}')
+        ax.axison = False
+
+    plt.tight_layout()
+    plt.show()
