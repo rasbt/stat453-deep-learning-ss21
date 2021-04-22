9efc76726328c25bf158d1a910e9eb3fee31c4b7
rasbt <mail@sebastianraschka.com>
2021-04-22 13:41:45 -0500

    gan lecture

---
+++ L18/helper_data.py
@@ -0,0 +1,282 @@
+import torch
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch.utils.data import SubsetRandomSampler
+from torch.utils.data import sampler
+
+
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Parameters:
+        ------------
+        tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        
+        Returns:
+        ------------
+        Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+        return tensor
+
+
+class ChunkSampler(sampler.Sampler):
+    """Samples elements sequentially from some offset. 
+    Arguments:
+        num_samples: # of desired datapoints
+        start: offset where we should start selecting from
+    """
+    def __init__(self, num_samples, start=0):
+        self.num_samples = num_samples
+        self.start = start
+
+    def __iter__(self):
+        return iter(range(self.start, self.start + self.num_samples))
+
+    def __len__(self):
+        return self.num_samples
+
+
+def get_dataloaders_cifar10(batch_size, num_workers=0,
+                            validation_fraction=None,
+                            train_transforms=None, test_transforms=None):
+
+    if train_transforms is None:
+        train_transforms = transforms.ToTensor()
+
+    if test_transforms is None:
+        test_transforms = transforms.ToTensor()
+
+    train_dataset = datasets.CIFAR10(root='data',
+                                     train=True,
+                                     transform=train_transforms,
+                                     download=True)
+
+    valid_dataset = datasets.CIFAR10(root='data',
+                                     train=True,
+                                     transform=test_transforms)
+
+    test_dataset = datasets.CIFAR10(root='data',
+                                    train=False,
+                                    transform=test_transforms)
+
+
+    if validation_fraction is not None:
+        num = int(validation_fraction * 50000)
+        train_indices = torch.arange(0, 50000 - num)
+        valid_indices = torch.arange(50000 - num, 50000)
+
+        #train_sampler = SubsetRandomSampler(train_indices)
+        #valid_sampler = SubsetRandomSampler(valid_indices)
+        train_sampler = ChunkSampler(train_indices.shape[0], 0)
+        valid_sampler = ChunkSampler(valid_indices.shape[0], train_indices.shape[0])
+
+        valid_loader = DataLoader(dataset=valid_dataset,
+                                  batch_size=batch_size,
+                                  num_workers=num_workers,
+                                  sampler=valid_sampler)
+
+        train_loader = DataLoader(dataset=train_dataset,
+                                  batch_size=batch_size,
+                                  num_workers=num_workers,
+                                  drop_last=True,
+                                  sampler=train_sampler)
+
+    else:
+        train_loader = DataLoader(dataset=train_dataset,
+                                  batch_size=batch_size,
+                                  num_workers=num_workers,
+                                  shuffle=True)
+
+    test_loader = DataLoader(dataset=test_dataset,
+                             batch_size=batch_size,
+                             num_workers=num_workers,
+                             shuffle=False)
+
+    if validation_fraction is None:
+        return train_loader, test_loader
+    else:
+        return train_loader, valid_loader, test_loader
+
+
+def get_dataloaders_mnist(batch_size, num_workers=0,
+                          validation_fraction=None,
+                          train_transforms=None, test_transforms=None):
+
+    if train_transforms is None:
+        train_transforms = transforms.ToTensor()
+
+    if test_transforms is None:
+        test_transforms = transforms.ToTensor()
+
+    train_dataset = datasets.MNIST(root='data',
+                                   train=True,
+                                   transform=train_transforms,
+                                   download=True)
+
+    valid_dataset = datasets.MNIST(root='data',
+                                   train=True,
+                                   transform=test_transforms)
+
+    test_dataset = datasets.MNIST(root='data',
+                                  train=False,
+                                  transform=test_transforms)
+
+    if validation_fraction is not None:
+        num = int(validation_fraction * 60000)
+        train_indices = torch.arange(0, 60000 - num)
+        valid_indices = torch.arange(60000 - num, 60000)
+
+        train_sampler = SubsetRandomSampler(train_indices)
+        valid_sampler = SubsetRandomSampler(valid_indices)
+
+        valid_loader = DataLoader(dataset=valid_dataset,
+                                  batch_size=batch_size,
+                                  num_workers=num_workers,
+                                  sampler=valid_sampler)
+
+        train_loader = DataLoader(dataset=train_dataset,
+                                  batch_size=batch_size,
+                                  num_workers=num_workers,
+                                  drop_last=True,
+                                  sampler=train_sampler)
+    else:
+        train_loader = DataLoader(dataset=train_dataset,
+                                  batch_size=batch_size,
+                                  num_workers=num_workers,
+                                  shuffle=True)
+
+    test_loader = DataLoader(dataset=test_dataset,
+                             batch_size=batch_size,
+                             num_workers=num_workers,
+                             shuffle=False)
+
+    if validation_fraction is None:
+        return train_loader, test_loader
+    else:
+        return train_loader, valid_loader, test_loader
+
+    
+def get_dataloaders_celeba(batch_size, num_workers=0,
+                           train_transforms=None,
+                           test_transforms=None,
+                           download=True):
+    """Targets are 40-dim vectors representing
+    00 - 5_o_Clock_Shadow
+    01 - Arched_Eyebrows
+    02 - Attractive 
+    03 - Bags_Under_Eyes
+    04 - Bald
+    05 - Bangs
+    06 - Big_Lips
+    07 - Big_Nose
+    08 - Black_Hair
+    09 - Blond_Hair
+    10 - Blurry 
+    11 - Brown_Hair 
+    12 - Bushy_Eyebrows 
+    13 - Chubby 
+    14 - Double_Chin 
+    15 - Eyeglasses 
+    16 - Goatee 
+    17 - Gray_Hair 
+    18 - Heavy_Makeup 
+    19 - High_Cheekbones 
+    20 - Male 
+    21 - Mouth_Slightly_Open 
+    22 - Mustache 
+    23 - Narrow_Eyes 
+    24 - No_Beard 
+    25 - Oval_Face 
+    26 - Pale_Skin 
+    27 - Pointy_Nose 
+    28 - Receding_Hairline 
+    29 - Rosy_Cheeks 
+    30 - Sideburns 
+    31 - Smiling 
+    32 - Straight_Hair 
+    33 - Wavy_Hair 
+    34 - Wearing_Earrings 
+    35 - Wearing_Hat 
+    36 - Wearing_Lipstick 
+    37 - Wearing_Necklace 
+    38 - Wearing_Necktie 
+    39 - Young         
+    """
+
+    if train_transforms is None:
+        train_transforms = transforms.ToTensor()
+
+    if test_transforms is None:
+        test_transforms = transforms.ToTensor()
+
+    train_dataset = datasets.CelebA(root='data',
+                                    split='train',
+                                    transform=train_transforms,
+                                    download=download)
+
+    valid_dataset = datasets.CelebA(root='data',
+                                    split='valid',
+                                    transform=test_transforms)
+
+    test_dataset = datasets.CelebA(root='data',
+                                   split='test',
+                                   transform=test_transforms)
+
+
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=batch_size,
+                              num_workers=num_workers,
+                              shuffle=True)
+
+    valid_loader = DataLoader(dataset=test_dataset,
+                             batch_size=batch_size,
+                             num_workers=num_workers,
+                             shuffle=False)
+    
+    test_loader = DataLoader(dataset=test_dataset,
+                             batch_size=batch_size,
+                             num_workers=num_workers,
+                             shuffle=False)
+
+    return train_loader, valid_loader, test_loader
+
+
+def compute_average_faces(feature_idx, image_dim, data_loader, device=None, encoding_fn=None):
+
+    avg_img_with_feat = torch.zeros(image_dim, dtype=torch.float32)
+    avg_img_without_feat = torch.zeros(image_dim, dtype=torch.float32)
+
+    num_img_with_feat = 0
+    num_images_without_feat = 0
+
+    for images, labels in data_loader:  
+        idx_img_with_feat = labels[:, feature_idx].to(torch.bool)
+
+        if encoding_fn is None:
+            embeddings = images
+        else:
+            ####################################
+            ### Get latent representation
+            with torch.no_grad():
+
+                if device is not None:
+                    images = images.to(device)
+                embeddings = encoding_fn(images).to('cpu')
+            ####################################    
+
+        avg_img_with_feat += torch.sum(embeddings[idx_img_with_feat], axis=0)
+        avg_img_without_feat += torch.sum(embeddings[~idx_img_with_feat], axis=0)
+        num_img_with_feat += idx_img_with_feat.sum(axis=0)
+        num_images_without_feat += (~idx_img_with_feat).sum(axis=0)
+
+    avg_img_with_feat /= num_img_with_feat
+    avg_img_without_feat /= num_images_without_feat
+    
+    return avg_img_with_feat, avg_img_without_feat
---
+++ L18/helper_train.py
@@ -0,0 +1,323 @@
+from helper_evaluate import compute_accuracy
+from helper_evaluate import compute_epoch_loss_classifier
+from helper_evaluate import compute_epoch_loss_autoencoder
+
+import time
+import torch
+import torch.nn.functional as F
+import torchvision
+
+
+def train_classifier_simple_v1(num_epochs, model, optimizer, device,
+                               train_loader, valid_loader=None,
+                               loss_fn=None, logging_interval=100,
+                               skip_epoch_stats=False):
+
+    log_dict = {'train_loss_per_batch': [],
+                'train_acc_per_epoch': [],
+                'train_loss_per_epoch': [],
+                'valid_acc_per_epoch': [],
+                'valid_loss_per_epoch': []}
+
+    if loss_fn is None:
+        loss_fn = F.cross_entropy
+
+    start_time = time.time()
+    for epoch in range(num_epochs):
+
+        model.train()
+        for batch_idx, (features, targets) in enumerate(train_loader):
+
+            features = features.to(device)
+            targets = targets.to(device)
+
+            # FORWARD AND BACK PROP
+            logits = model(features)
+            loss = loss_fn(logits, targets)
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            # UPDATE MODEL PARAMETERS
+            optimizer.step()
+
+            # LOGGING
+            log_dict['train_loss_per_batch'].append(loss.item())
+
+            if not batch_idx % logging_interval:
+                print('Epoch: %03d/%03d | Batch %04d/%04d | Loss: %.4f'
+                      % (epoch+1, num_epochs, batch_idx,
+                          len(train_loader), loss))
+
+        if not skip_epoch_stats:
+            model.eval()
+
+            with torch.set_grad_enabled(False):  # save memory during inference
+
+                train_acc = compute_accuracy(model, train_loader, device)
+                train_loss = compute_epoch_loss_classifier(
+                    model, train_loader, loss_fn, device)
+                print('***Epoch: %03d/%03d | Train. Acc.: %.3f%% | Loss: %.3f' % (
+                      epoch+1, num_epochs, train_acc, train_loss))
+                log_dict['train_loss_per_epoch'].append(train_loss.item())
+                log_dict['train_acc_per_epoch'].append(train_acc.item())
+
+                if valid_loader is not None:
+                    valid_acc = compute_accuracy(model, valid_loader, device)
+                    valid_loss = compute_epoch_loss_classifier(
+                        model, valid_loader, loss_fn, device)
+                    print('***Epoch: %03d/%03d | Valid. Acc.: %.3f%% | Loss: %.3f' % (
+                          epoch+1, num_epochs, valid_acc, valid_loss))
+                    log_dict['valid_loss_per_epoch'].append(valid_loss.item())
+                    log_dict['valid_acc_per_epoch'].append(valid_acc.item())
+
+        print('Time elapsed: %.2f min' % ((time.time() - start_time)/60))
+
+    print('Total Training Time: %.2f min' % ((time.time() - start_time)/60))
+
+    return log_dict
+
+
+def train_autoencoder_v1(num_epochs, model, optimizer, device,
+                         train_loader, loss_fn=None,
+                         logging_interval=100,
+                         skip_epoch_stats=False,
+                         save_model=None):
+
+    log_dict = {'train_loss_per_batch': [],
+                'train_loss_per_epoch': []}
+
+    if loss_fn is None:
+        loss_fn = F.mse_loss
+
+    start_time = time.time()
+    for epoch in range(num_epochs):
+
+        model.train()
+        for batch_idx, (features, _) in enumerate(train_loader):
+
+            features = features.to(device)
+
+            # FORWARD AND BACK PROP
+            logits = model(features)
+            loss = loss_fn(logits, features)
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            # UPDATE MODEL PARAMETERS
+            optimizer.step()
+
+            # LOGGING
+            log_dict['train_loss_per_batch'].append(loss.item())
+
+            if not batch_idx % logging_interval:
+                print('Epoch: %03d/%03d | Batch %04d/%04d | Loss: %.4f'
+                      % (epoch+1, num_epochs, batch_idx,
+                          len(train_loader), loss))
+
+        if not skip_epoch_stats:
+            model.eval()
+
+            with torch.set_grad_enabled(False):  # save memory during inference
+
+                train_loss = compute_epoch_loss_autoencoder(
+                    model, train_loader, loss_fn, device)
+                print('***Epoch: %03d/%03d | Loss: %.3f' % (
+                      epoch+1, num_epochs, train_loss))
+                log_dict['train_loss_per_epoch'].append(train_loss.item())
+
+        print('Time elapsed: %.2f min' % ((time.time() - start_time)/60))
+
+    print('Total Training Time: %.2f min' % ((time.time() - start_time)/60))
+    if save_model is not None:
+        torch.save(model.state_dict(), save_model)
+
+    return log_dict
+
+
+def train_vae_v1(num_epochs, model, optimizer, device, 
+                 train_loader, loss_fn=None,
+                 logging_interval=100, 
+                 skip_epoch_stats=False,
+                 reconstruction_term_weight=1,
+                 save_model=None):
+
+    log_dict = {'train_combined_loss_per_batch': [],
+                'train_combined_loss_per_epoch': [],
+                'train_reconstruction_loss_per_batch': [],
+                'train_kl_loss_per_batch': []}
+
+    if loss_fn is None:
+        loss_fn = F.mse_loss
+
+    start_time = time.time()
+    for epoch in range(num_epochs):
+
+        model.train()
+        for batch_idx, (features, _) in enumerate(train_loader):
+
+            features = features.to(device)
+
+            # FORWARD AND BACK PROP
+            encoded, z_mean, z_log_var, decoded = model(features)
+
+            # total loss = reconstruction loss + KL divergence
+            # kl_divergence = (0.5 * (z_mean**2 +
+            #                         torch.exp(z_log_var) - z_log_var - 1)).sum()
+            kl_div = -0.5 * torch.sum(1 + z_log_var 
+                                      - z_mean**2 
+                                      - torch.exp(z_log_var), 
+                                      axis=1) # sum over latent dimension
+
+            batchsize = kl_div.size(0)
+            kl_div = kl_div.mean() # average over batch dimension
+    
+            pixelwise = loss_fn(decoded, features, reduction='none')
+            pixelwise = pixelwise.view(batchsize, -1).sum(axis=1) # sum over pixels
+            pixelwise = pixelwise.mean() # average over batch dimension
+            
+            loss = reconstruction_term_weight*pixelwise + kl_div
+            
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            # UPDATE MODEL PARAMETERS
+            optimizer.step()
+
+            # LOGGING
+            log_dict['train_combined_loss_per_batch'].append(loss.item())
+            log_dict['train_reconstruction_loss_per_batch'].append(pixelwise.item())
+            log_dict['train_kl_loss_per_batch'].append(kl_div.item())
+            
+            if not batch_idx % logging_interval:
+                print('Epoch: %03d/%03d | Batch %04d/%04d | Loss: %.4f'
+                      % (epoch+1, num_epochs, batch_idx,
+                          len(train_loader), loss))
+
+        if not skip_epoch_stats:
+            model.eval()
+            
+            with torch.set_grad_enabled(False):  # save memory during inference
+                
+                train_loss = compute_epoch_loss_autoencoder(
+                    model, train_loader, loss_fn, device)
+                print('***Epoch: %03d/%03d | Loss: %.3f' % (
+                      epoch+1, num_epochs, train_loss))
+                log_dict['train_combined_per_epoch'].append(train_loss.item())
+
+        print('Time elapsed: %.2f min' % ((time.time() - start_time)/60))
+    
+    print('Total Training Time: %.2f min' % ((time.time() - start_time)/60))
+    if save_model is not None:
+        torch.save(model.state_dict(), save_model)
+    
+    return log_dict
+
+
+def train_gan_v1(num_epochs, model, optimizer_gen, optimizer_discr, 
+                 latent_dim, device, train_loader, loss_fn=None,
+                 logging_interval=100, 
+                 save_model=None):
+    
+    log_dict = {'train_generator_loss_per_batch': [],
+                'train_discriminator_loss_per_batch': [],
+                'train_discriminator_real_acc_per_batch': [],
+                'train_discriminator_fake_acc_per_batch': [],
+                'images_from_noise_per_epoch': []}
+
+    if loss_fn is None:
+        loss_fn = F.binary_cross_entropy_with_logits
+
+    # Batch of latent (noise) vectors for
+    # evaluating / visualizing the training progress
+    # of the generator
+    fixed_noise = torch.randn(64, latent_dim, 1, 1, device=device) # format NCHW
+
+    start_time = time.time()
+    for epoch in range(num_epochs):
+
+        model.train()
+        for batch_idx, (features, _) in enumerate(train_loader):
+
+            batch_size = features.size(0)
+
+            # real images
+            real_images = features.to(device)
+            real_labels = torch.ones(batch_size, device=device) # real label = 1
+
+            # generated (fake) images
+            noise = torch.randn(batch_size, latent_dim, 1, 1, device=device)  # format NCHW
+            fake_images = model.generator_forward(noise)
+            fake_labels = torch.zeros(batch_size, device=device) # fake label = 0
+            flipped_fake_labels = real_labels # here, fake label = 1
+
+            # --------------------------
+            # Train Discriminator
+            # --------------------------
+
+            optimizer_discr.zero_grad()
+
+            # get discriminator loss on real images
+            discr_pred_real = model.discriminator_forward(real_images).view(-1) # Nx1 -> N
+            real_loss = loss_fn(discr_pred_real, real_labels)
+            # real_loss.backward()
+
+            # get discriminator loss on fake images
+            discr_pred_fake = model.discriminator_forward(fake_images.detach()).view(-1)
+            fake_loss = loss_fn(discr_pred_fake, fake_labels)
+            # fake_loss.backward()
+
+            # combined loss
+            discr_loss = 0.5*(real_loss + fake_loss)
+            discr_loss.backward()
+
+            optimizer_discr.step()
+
+            # --------------------------
+            # Train Generator
+            # --------------------------
+
+            optimizer_gen.zero_grad()
+
+            # get discriminator loss on fake images with flipped labels
+            discr_pred_fake = model.discriminator_forward(fake_images).view(-1)
+            gener_loss = loss_fn(discr_pred_fake, flipped_fake_labels)
+            gener_loss.backward()
+
+            optimizer_gen.step()
+
+            # --------------------------
+            # Logging
+            # --------------------------   
+            log_dict['train_generator_loss_per_batch'].append(gener_loss.item())
+            log_dict['train_discriminator_loss_per_batch'].append(discr_loss.item())
+            
+            predicted_labels_real = torch.where(discr_pred_real.detach() > 0., 1., 0.)
+            predicted_labels_fake = torch.where(discr_pred_fake.detach() > 0., 1., 0.)
+            acc_real = (predicted_labels_real == real_labels).float().mean()*100.
+            acc_fake = (predicted_labels_fake == fake_labels).float().mean()*100.
+            log_dict['train_discriminator_real_acc_per_batch'].append(acc_real.item())
+            log_dict['train_discriminator_fake_acc_per_batch'].append(acc_fake.item())         
+            
+            if not batch_idx % logging_interval:
+                print('Epoch: %03d/%03d | Batch %03d/%03d | Gen/Dis Loss: %.4f/%.4f' 
+                       % (epoch+1, num_epochs, batch_idx, 
+                          len(train_loader), gener_loss.item(), discr_loss.item()))
+
+        ### Save images for evaluation
+        with torch.no_grad():
+            fake_images = model.generator_forward(fixed_noise).detach().cpu()
+            log_dict['images_from_noise_per_epoch'].append(
+                torchvision.utils.make_grid(fake_images, padding=2, normalize=True))
+
+
+        print('Time elapsed: %.2f min' % ((time.time() - start_time)/60))
+    
+    print('Total Training Time: %.2f min' % ((time.time() - start_time)/60))
+    
+    if save_model is not None:
+        torch.save(model.state_dict(), save_model)
+    
+    return log_dict
